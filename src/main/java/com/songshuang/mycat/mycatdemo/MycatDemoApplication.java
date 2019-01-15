fd114fe72e89acb74bc8773dee2f32634ab65d3e
songshuang <songshuang@babytree-inc.com>
2019-01-15 19:39:53 +0800

    first commit

---
+++ src/main/java/com/songshuang/mycat/mycatdemo/MycatDemoApplication.java
@@ -0,0 +1,14 @@
+package com.songshuang.mycat.mycatdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycatDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycatDemoApplication.class, args);
+	}
+
+}
+
